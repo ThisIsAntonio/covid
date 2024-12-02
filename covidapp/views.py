e2c78de5615a842326be966eefa66284ee7b8798
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-12-02 14:31:28 -0500

    adding variables between view-templates

--- covidapp/views.py
+++ covidapp/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def helloworldview(request):
-    return render(request, 'helloworld.html')
+    string = "Everyone"
+    context2 = {}
+    
+    mylistitems = ['item1', 'item2', 'item3']
+    context =   { 
+                    'string': string ,
+                    'mylistitems': mylistitems 
+                }
+    return render(request, 'helloworld.html', context)
 
